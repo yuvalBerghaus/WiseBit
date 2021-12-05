10406fc6a95d3812b48c60d90c438fcd057c756e
karina <karina4nosenko@gmail.com>
2021-12-05 20:47:41 +0200

    removed users json

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,24 +1,8 @@
-const users = require('../models/users.json');
-
-const findUser = async (email, password) => {
-    const user = await users.find(user => (user.email === email)&&(user.password === password));
-    if(user) {
-        return user;
-    } else {
-        return null;
-    }
-};
-
 exports.authController = {
-    loginUser(req, res) {
-        const { email, password } = req.body;
-        findUser(email, password)
-            .then(user => {
-                if(user) {
-                    res.status(200).json(user);
-                } else {
-                    res.status(200).json({'error':'unauthorized'});
-                }
-            })
+    getSignIn(req, res) {
+        res.status(200).sendFile('../index.js', { root: __dirname });
+    },
+    getSignUp(req, res) {
+        res.status(200).sendFile('../signUp.js', { root: __dirname });
     }
 };
