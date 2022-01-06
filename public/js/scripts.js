1b41b58029bf40755f9543268380234f7fc72dd4
karina <karina4nosenko@gmail.com>
2022-01-06 22:58:55 +0200

    Added categories ui in the homepage

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -99,6 +99,63 @@ const updateGraph = () => {
 
 updateGraph();
 
+/* Categories */
+const getCategoryNameByIndex = (index) => {
+    if(index == 0)  return 'Home';
+    if(index == 1)  return 'Grocery';
+    if(index == 2)  return 'Hangout';
+    if(index == 3)  return 'Patrol';
+};
+
+const renderCategories = () => {
+    fetch(`${serviceUrl}/api/auth/userid`, { credentials: 'include' })
+    .then(response => {
+        return response.text();
+    }).then(data => {
+        fetch(`${serviceUrl}/api/users/${data}`)
+        .then(response => {
+            return response.text();
+        })
+        .then(str => {
+            const user = JSON.parse(str);
+
+            user.expenses.map((expense, index) => {
+                $('#categories').append(
+                    `
+                        <div class="category">
+                            <div class="single-chart">
+                                <svg viewBox="0 0 36 36" class="circular-chart ${index % 2 == 0 ? 'green' : 'blue'}">
+                                <path class="circle-bg"
+                                    d="M18 2.0845
+                                    a 15.9155 15.9155 0 0 1 0 31.831
+                                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                                />
+                                <path class="circle"
+                                    stroke-dasharray="${expense.amount_spent/user.allowed_budget[index].sum*100}, 100"
+                                    d="M18 2.0845
+                                    a 15.9155 15.9155 0 0 1 0 31.831
+                                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                                />
+                                <text x="18" y="20.35" class="percentage">${expense.amount_spent/user.allowed_budget[index].sum*100}%</text>
+                                </svg>
+                            </div>
+                            <div class="infoAndExpense">
+                                <div class="categoryInfo">
+                                    <p>${getCategoryNameByIndex(index)}</p>
+                                    <p>${expense.amount_spent/user.allowed_budget[index].sum*100}% of budget</p>
+                                </div>
+                                <p class="expense">$${expense.amount_spent}</p>
+                            </div>
+                        </div>
+                    `
+                )
+            })
+ 
+        })
+    })
+}
+renderCategories();
+
 /* Forms input animations */
 
 const inputs = document.querySelectorAll(".input");
